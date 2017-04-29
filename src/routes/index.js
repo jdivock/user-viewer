223ab698a4ae90070b9409d3dc671d2581497775
Jason Divock <jdivock@gmail.com>
2017-04-28 19:53:59 -0700

    removed boilerplate n stuff

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,32 +1,10 @@
-// We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/CoreLayout';
 import UserViewer from './UserViewer';
-
-/*  Note: Instead of using JSX, we recommend using react-router
-    PlainRoute objects to build route definitions.   */
 
 export const createRoutes = () => ({
   path        : '/',
   component   : CoreLayout,
   indexRoute  : UserViewer,
 });
-
-/*  Note: childRoutes can be chunked or otherwise loaded programmatically
-    using getChildRoutes with the following signature:
-
-    getChildRoutes (location, cb) {
-      require.ensure([], (require) => {
-        cb(null, [
-          // Remove imports!
-          require('./Counter').default(store)
-        ])
-      })
-    }
-
-    However, this is not necessary for code-splitting! It simply provides
-    an API for async route definitions. Your code splitting should occur
-    inside the route `getComponent` function, since it is only invoked
-    when the route exists and matches.
-*/
 
 export default createRoutes;
